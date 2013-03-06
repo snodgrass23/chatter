887c507bc2312ff7e5e8227c4c805371eaa4a9ec
Jim Snodgrass <jim@skookum.com>
2013-03-06 14:02:14 -0500

    working client and server test apps

--- client.js
+++ client.js
@@ -8,6 +8,19 @@ chatter_client.on('new_message', function (message) {
   if (typeof message == "string") {
     message = JSON.parse(message);
   }
-  console.log("new message: ", message.length, message);
+  console.log(message.body);
 });
 
+process.nextTick(waitForMessage);
+
+function waitForMessage() {
+  stdin.resume();
+  stdin.setEncoding('utf8');
+
+  // when data is submitted by user
+  stdin.on('data', sendMessage);
+}
+
+function sendMessage(message) {
+  chatter_client.send(message.replace("\n", ""));
+}
