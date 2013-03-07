965be3dabf9047cbd9670d2feb862dc57d666d83
Jim Snodgrass <jim@skookum.com>
2013-03-06 21:50:06 -0500

    more awesomeness for the CLI client

--- examples/client.js
+++ examples/client.js
@@ -1,7 +1,8 @@
 var chatter = require('../index'),
     stdin = process.stdin,
     stdout = process.stdout,
-    username = "";
+    username = "",
+    buffer = "";
 
 var readline = require('readline');
 
@@ -29,14 +30,35 @@ rl.question("\033[35m Enter Your Usename: \033[39m ", function(answer) {
 function waitForMessage() {
   stdin.resume();
   stdin.setEncoding('utf8');
+  stdin.setRawMode(true);
 
   writeUsername();
 
-  // when data is submitted by user
-  stdin.on('data', sendMessage);
+  stdin.on('keypress', function (chunk, key) {
+
+    if (key && key.ctrl && key.name == 'c') return process.exit();
+
+    // handle
+    if (key && (key.name == 'backspace' || key.name == 'delete')) {
+      if (buffer.length > 0) {
+        buffer = buffer.slice(0, buffer.length - 1);
+        return stdout.write("\033[1D \033[1D");
+      }
+      else {
+        return;
+      }
+    }
+
+    if (key && key.name == "enter") return sendMessage(buffer);
+
+    buffer += chunk;
+    stdout.write(chunk);
+  });
 }
 
 function sendMessage(message) {
+  buffer = "";
+  stdout.write("\n");
   writeUsername();
   chatter_client.send(message.replace("\n", ""), username);
 }
@@ -58,6 +80,7 @@ function writeMessage(message, user) {
   stdout.write("\r\033[0K");
   console.log("\033[32m" + message.user + "\033[39m : " + message.body);
   writeUsername();
+  stdout.write(buffer);
 }
 
 function writeUsername() {
